4c367544ea7cb5a096b2eac55e2c685c175551eb
IwonaGorska <iwonagorskaa@gmail.com>
2020-11-29 21:11:48 +0100

    Added endpoints

---
+++ back-nodejs/queries.js
@@ -0,0 +1,82 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'postgres',
+  port: 5432,
+})
+
+const getItems = (request, response) => {
+    pool.query('SELECT * FROM items ORDER BY item_id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+ }
+ 
+ const getItemById = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('SELECT * FROM items WHERE item_id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+ }
+ 
+ const createItem = (request, response) => {
+    const { name } = request.body
+  
+    pool.query('INSERT INTO items (name) VALUES ($1)', [name], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`Item added with ID: ${result.insertId}`)
+    })
+  }
+ 
+ const updateItem = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { name } = request.body
+  
+    pool.query(
+      'UPDATE items SET name = $1 WHERE item_id = $2',
+      [name, id],
+      (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).send(`Item modified with ID: ${id}`)
+      }
+    )
+ }
+ 
+ const deleteItem = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('DELETE FROM items WHERE item_id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Item deleted with ID: ${id}`)
+    })
+ }
+
+ module.exports = {
+    getItems,
+    getItemById,
+    createItem,
+    updateItem,
+    deleteItem,
+  }
+
+
+  /*
+  Run command:
+  Remove-item alias:curl
+  when error with Invoke-WebRequest happened
+  Best tutorial: https://blog.logrocket.com/setting-up-a-restful-api-with-node-js-and-postgresql-d96d6fc892d8/
+  */ 
